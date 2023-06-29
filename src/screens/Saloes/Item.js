1a0d9d8e4891efa9f4a318581313d6db5b1586e1
JeanMaxSKrebs <jeanmaxskrebs@gmail.com>
2023-06-29 15:58:19 -0300

    add: botao visualizar

--- src/screens/Saloes/Item.js
+++ src/screens/Saloes/Item.js
@@ -1,5 +1,6 @@
-import React, { useRef , useState} from 'react';
-import { View, Image, Text, StyleSheet  } from 'react-native';
+/* eslint-disable react/no-unstable-nested-components */
+import React, {useRef, useState} from 'react';
+import {View, Image, Text, StyleSheet} from 'react-native';
 import styled from 'styled-components/native';
 import {COLORS} from '../../assets/colors';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -36,9 +37,9 @@ const Botao = styled.TouchableHighlight`
   height: auto;
   /* margin: 0 0 0 0px; */
   padding: 0 40px 0 40px;
-  background-color: ${({ active }) => (active ? COLORS.primary : COLORS.terciary)};
+  background-color: ${({active}) =>
+    active ? COLORS.primary : COLORS.terciary};
   border-radius: 30px;
-
 `;
 
 const Opcoes = styled.View`
@@ -49,8 +50,6 @@ const Opcoes = styled.View`
   height: auto;
   justify-content: center;
   border-radius: 30px;
-
-  
 `;
 const Container = styled.View`
   color: black;
@@ -83,88 +82,107 @@ const TextCidade = styled.Text`
   margin-left: 5px;
 `;
 
-
 const Item = ({item, onPress}) => {
   const [activeButton, setActiveButton] = React.useState(null);
   const [images, setImages] = useState([]);
   const [activeSlideIndex, setActiveSlideIndex] = useState(0);
 
-/**/
+  /**/
 
-  const handleButtonPress = (button) => {
+  const handleButtonPress = button => {
     setActiveButton(button);
     console.log('button');
     console.log(button);
   };
-/**/
-  
-  const handleSlideChange = (index) => {
+  /**/
+
+  const handleSlideChange = index => {
     setActiveSlideIndex(index);
     // console.log('Slide alterado:', index+1);
   };
 
-
   const CustomPrevButton = () => {
     return <Text style={styles.prevButton}>‹</Text>;
   };
-  
+
   const CustomNextButton = () => {
     return <Text style={styles.nextButton}>›</Text>;
   };
-  
+
   return (
     <>
       <Div>
         <Profile>
           <Button onPress={onPress} underlayColor="transparent">
             <>
-            <Icon name="person-circle-outline" size={60} color={COLORS.primaryDark} />
-            <TextNome>{item.nome}</TextNome>
-            </>          
+              <Icon
+                name="person-circle-outline"
+                size={60}
+                color={COLORS.primaryDark}
+              />
+              <TextNome>{item.nome}</TextNome>
+            </>
           </Button>
-          <Icon name="ellipsis-vertical" size={40} color="black" ></Icon>
+          <Icon name="ellipsis-vertical" size={40} color="black" />
         </Profile>
 
-        {activeButton === 'menu' ? <>
-        <Container  style={{ height: 300}}>
-          <TextDescricao>{item.descricao}</TextDescricao>
-          <TextCidade>Cidade: {item.cidade}</TextCidade>
-          <TextCapacidade>Capacidade: {item.capacidade}</TextCapacidade>
-        </Container>
-        </> :
-        <Container style={{ height: 300}}>
-          <Swiper index={0} style={styles.wrapper}
-                  onIndexChanged={handleSlideChange}
-                  showsButtons={true}
-                  prevButton={<CustomPrevButton />}
-                  nextButton={<CustomNextButton />}
-                  >
-
-            {/* {images.map((image, index) => ( */}
-            {item.imagens.map((image, index) => (
-              <>
-              <View style={styles.slide} key={index}>
-                  <Image source={{ uri: image }} key={image} style={{ width: 280, height: 250, borderRadius: 15 }} />
-              </View>
-              </>
-            ))}
-
-          </Swiper>
-          {/* <Text>Slide Atual: {activeSlideIndex+1}</Text> */}
-        </Container>
-        }
-        <Opcoes>
-          <Botao onPress={() => handleButtonPress('menu')}
-                  underlayColor="transparent"
-                  active={activeButton === 'menu'}
-              >
-              <Icon name="menu" size={60} color="black"></Icon>
-          </Botao>
-          <Botao onPress={() => handleButtonPress('image')}
+        {activeButton === 'menu' ? (
+          <>
+            <Container style={{height: 300}}>
+              <TextDescricao>{item.descricao}</TextDescricao>
+              <TextCidade>Cidade: {item.cidade}</TextCidade>
+              <TextCapacidade>Capacidade: {item.capacidade}</TextCapacidade>
+              <Botao
+                onPress={onPress}
                 underlayColor="transparent"
-                active={activeButton === 'image'}
-               >
-              <Icon name="image" size={60} color="black" ></Icon>
+                style={{backgroundColor: COLORS.primary}}>
+                <Text>Visualizar</Text>
+              </Botao>
+            </Container>
+          </>
+        ) : (
+          <Container style={{height: 300}}>
+            <Swiper
+              index={0}
+              style={styles.wrapper}
+              onIndexChanged={handleSlideChange}
+              showsButtons={true}
+              prevButton={<CustomPrevButton />}
+              nextButton={<CustomNextButton />}>
+              {/* {images.map((image, index) => ( */}
+              {item.imagens.map((image, index) => (
+                <>
+                  <View style={styles.slide} key={index}>
+                    <Image
+                      source={{uri: image}}
+                      key={image}
+                      style={{width: 280, height: 250, borderRadius: 15}}
+                    />
+                  </View>
+                </>
+              ))}
+            </Swiper>
+            <Botao
+              onPress={onPress}
+              underlayColor="transparent"
+              style={{backgroundColor: COLORS.primary}}>
+              <Text>Visualizar</Text>
+            </Botao>
+            {/* <Text>Slide Atual: {activeSlideIndex+1}</Text> */}
+          </Container>
+        )}
+        <Opcoes>
+          <Botao
+            onPress={() => handleButtonPress('menu')}
+            underlayColor="transparent"
+            active={activeButton === 'menu'}>
+            <Icon name="menu" size={60} color="black" />
+          </Botao>
+          <Botao
+            onPress={() => handleButtonPress('image')}
+            underlayColor="transparent"
+            active={activeButton === 'image'}>
+            <Icon name="image" size={60} color="black" />
           </Botao>
         </Opcoes>
       </Div>
@@ -184,12 +202,11 @@ const styles = {
   },
 
   container: {
-    flex: 1
+    flex: 1,
   },
 
-  wrapper: {
-  },
-  
+  wrapper: {},
+
   slide: {
     padding: 5,
   },
