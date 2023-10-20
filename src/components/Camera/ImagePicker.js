44f609efdfe04b25ca359af605f6cf8558d6be39
JeanMaxSKrebs <jeanmaxskrebs@gmail.com>
2023-10-20 13:45:58 -0300

    feat: atualizar logo e add component camera

---
+++ src/components/Camera/ImagePicker.js
@@ -0,0 +1,119 @@
+// ImagePicker.js
+
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, Button } from 'react-native';
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+import Icon from 'react-native-vector-icons/Ionicons';
+import MeuButtonMetade from '../MeuButtonMetade';
+import Texto from '../Texto';
+
+
+const ImagePicker = ({ onPress }) => {
+    const [imageUri, setImageUri] = useState(null);
+
+    const handleLaunchCamera = () => {
+        const options = {
+            mediaType: 'photo', // Define o tipo de mídia (foto)
+            quality: 0.5, // Define a qualidade da imagem (0 a 1)
+        };
+
+        launchCamera(options, (response) => {
+            console.log('camera');
+
+            console.log(options);
+            console.log(response);
+            if (response.didCancel) {
+                console.log('Usuário cancelou a captura de imagem');
+            } else if (response.error) {
+                console.log('Erro ao capturar imagem:', response.error);
+            } else {
+                const selectedImageUri = response.assets[0].uri;
+                setImageUri(selectedImageUri);
+                // Chame a função de callback com o URI da imagem selecionada
+                onPress(selectedImageUri);
+            }
+        });
+    };
+
+    const handleLaunchImageLibrary = () => {
+        const options = {
+            mediaType: 'photo', // Define o tipo de mídia (foto)
+            quality: 0.5, // Define a qualidade da imagem (0 a 1)
+        };
+
+        launchImageLibrary(options, (response) => {
+            console.log('library');
+
+            console.log(options);
+            console.log(response);
+            if (response.didCancel) {
+                console.log('Usuário cancelou a seleção de imagem');
+            } else if (response.error) {
+                console.log('Erro ao selecionar imagem:', response.error);
+            } else {
+                const selectedImageUri = response.assets[0].uri;
+                setImageUri(selectedImageUri);
+                // Chame a função de callback com o URI da imagem selecionada
+                onPress(selectedImageUri);
+            }
+        });
+    };
+
+    const zerar = () => {
+        setImageUri(null)
+    };
+
+    const renderPlaceholder = (value) => {
+        if (value !== '') {
+            return <Text style={{
+                textAlign: 'left',
+                fontSize: 10,
+                marginBottom: 1,
+                color: 'gray'
+            }}>
+                {value}
+            </Text>
+
+        }
+        return null;
+    };
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            {imageUri !== undefined && imageUri !== null
+                ? (
+                    <View>
+                        <View style={{ width: '65%', marginTop: 10 }}  >
+                            {renderPlaceholder(imageUri ? 'Nova Logo' : '')}
+                        </View>
+                        <Image
+                            style={{ width: 200, height: 200 }} source={{ uri: imageUri }}
+                        />
+
+                    </View>
+                ) : (
+                    <View>
+                        <Text style={{ width: '65%', marginTop: 10 }} >
+                            {renderPlaceholder(imageUri ? 'Nova Logo' : '')}
+
+                        </Text>
+                    </View>
+                )
+            }
+            <View style={{ flexDirection: 'row' }}>
+                <MeuButtonMetade width={'auto'} texto="Capturar Foto" onClick={handleLaunchCamera}
+                />
+                <MeuButtonMetade width={'auto'} texto="Selecionar Foto" onClick={handleLaunchImageLibrary}
+                />
+                {imageUri !== undefined && imageUri !== null &&
+                    <MeuButtonMetade width={'auto'} cor={'red'} texto="X"
+                        onClick={zerar}
+                    />
+                }
+            </View >
+
+        </View >
+    );
+};
+
+export default ImagePicker;
--- src/screens/AtualizarDados/index.js
+++ src/screens/AtualizarDados/index.js
@@ -1,37 +1,42 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {Alert, ToastAndroid, StyleSheet, View } from 'react-native';
-import {Container, TextInput, Text, TextPlaceholder} from './styles';
+import React, { useContext, useEffect, useState } from 'react';
+import { Alert, ToastAndroid, Image, View, TouchableOpacity, ScrollViewComponent, ScrollView } from 'react-native';
+import { Container, TextInput, Text, TextPlaceholder } from './styles';
 import MeuButton from '../../components/MeuButton';
 import DeleteButton from '../../components/DeleteButton';
 import Loading from '../../components/Loading';
-import {AuthUserContext} from '../../context/AuthUserProvider';
-import {ProfileContext} from '../../context/ProfileProvider';
-import {CommonActions} from '@react-navigation/native';
-import {COLORS} from '../../assets/colors';
+import { AuthUserContext } from '../../context/AuthUserProvider';
+import { ProfileContext } from '../../context/ProfileProvider';
+import { CommonActions } from '@react-navigation/native';
+import { COLORS } from '../../assets/colors';
 import { SalaoContext } from '../../context/SalaoProvider';
+import ImagePicker from '../../components/Camera/ImagePicker';
+import Texto from '../../components/Texto';
 
-const AtualizarDados = ({route, navigation}) => {
+const AtualizarDados = ({ route, navigation }) => {
   const [id, setId] = useState('');
   const [nome, setNome] = useState('');
+  const [logo, setLogo] = useState(null);
+  const [newLogo, setNewLogo] = useState(null);
   const [cidade, setCidade] = useState('');
   const [endereco, setEndereco] = useState('');
   const [descricao, setDescricao] = useState('');
-  const [capacidade, setCapacidade] = useState(0); 
+  const [capacidade, setCapacidade] = useState(0);
 
   const [loading, setLoading] = useState(false);
-  const {updateSalao} = useContext(SalaoContext);
-  console.log(route.params)
+  const { updateSalao } = useContext(SalaoContext);
+  // console.log(route.params)
   let salao = route.params.salao;
-  console.log(salao)
+  // console.log(salao)
   useEffect(() => {
     if (salao) {
-      console.log(salao)
+      // console.log(salao)
       setId(salao.id);
       setNome(salao.nome);
       setCidade(salao.cidade);
       setEndereco(salao.endereco);
       setDescricao(salao.descricao);
       setCapacidade(salao.capacidade);
+      setLogo(salao.logo);
     }
   }, [salao]);
 
@@ -39,11 +44,14 @@ const AtualizarDados = ({route, navigation}) => {
     const newSalao = {
       id: id,
       nome: nome,
+      logo: newLogo ? newLogo : logo,
       cidade: cidade,
       endereco: endereco,
       descricao: descricao,
       capacidade: capacidade,
     };
+    console.log('newSalao');
+    console.log(newSalao);
     updateSalao(newSalao);
     navigation.goBack()
   };
@@ -55,69 +63,99 @@ const AtualizarDados = ({route, navigation}) => {
     return null;
   };
 
+
+  const handleImageSelected = (imageUri) => {
+    console.log('imageUri');
+    console.log(imageUri);
+    setNewLogo(imageUri);
+  };
+
   return (
-    <Container>
-      <Text style={{ color: 'black', marginBottom: 20 }} >Atualizar Dados do Salão</Text>
-      <View  style={{ width: '65%', height: 20 }} >
-      {renderPlaceholder(nome ? 'Nome' : '')}
-      </View>
-      <TextInput
-        placeholderTextColor="gray"
-        value={nome}
-        placeholder="Nome"
-        keyboardType="default"
-        returnKeyType="next"
-        onChangeText={t => setNome(t)}
+
+    <ScrollView>
+      <Container>
+        <Text style={{ color: 'black', marginBottom: 20 }} >Atualizar Dados do Salão</Text>
+
+        {logo !== undefined && logo !== null
+          && (
+            <View>
+              <View style={{ width: '65%' }} >
+                {renderPlaceholder(logo ? 'Logo' : '')}
+              </View>
+              <View style={{ borderWidth: 3, borderColor: 'black', borderRadius: 10 }}>
+                <Image
+                  style={{ width: 300, height: 300 }}
+                  source={{ uri: logo }}
+                  resizeMode="cover"
+                />
+              </View>
+
+            </View>
+          )
+        }
+        <ImagePicker onPress={handleImageSelected} />
+
+
+        <View style={{ width: '65%', marginTop: 30 }} >
+          {renderPlaceholder(nome ? 'Nome' : '')}
+        </View>
+        <TextInput
+          placeholderTextColor="gray"
+          value={nome}
+          placeholder="Nome"
+          keyboardType="default"
+          returnKeyType="next"
+          onChangeText={t => setNome(t)}
         />
-      <View  style={{ width: '65%' }} >
-      {renderPlaceholder(descricao ? 'Descrição' : '')}
-      </View>
-      <TextInput
-        placeholderTextColor="gray"
-        value={descricao}
-        placeholder="Descrição"
-        keyboardType="default"
-        returnKeyType="next"
-        onChangeText={t => setDescricao(t)}
+        <View style={{ width: '65%' }} >
+          {renderPlaceholder(descricao ? 'Descrição' : '')}
+        </View>
+        <TextInput
+          placeholderTextColor="gray"
+          value={descricao}
+          placeholder="Descrição"
+          keyboardType="default"
+          returnKeyType="next"
+          onChangeText={t => setDescricao(t)}
         />
-      <View  style={{ width: '65%' }} >
-        {console.log(capacidade)}
-      {renderPlaceholder(capacidade ? 'Capacidade' : '')}
-      </View>
-      <TextInput
-        placeholderTextColor="gray"
-        value={capacidade.toString()}
-        placeholder="Capacidade"
-        keyboardType="numeric"
-        returnKeyType="done"
-        onChangeText={t => setCapacidade(t)}
+        <View style={{ width: '65%' }} >
+          {renderPlaceholder(capacidade ? 'Capacidade' : '')}
+        </View>
+        <TextInput
+          placeholderTextColor="gray"
+          value={capacidade.toString()}
+          placeholder="Capacidade"
+          keyboardType="numeric"
+          returnKeyType="done"
+          onChangeText={t => setCapacidade(t)}
         />
-      <Text style={{ color: 'black', fontSize: 18 }} >Localização</Text>
-      <View  style={{ width: '65%' }} >
-      {renderPlaceholder(endereco ? 'Endereço' : '')}
-      </View>
-      <TextInput
-        placeholderTextColor="gray"
-        value={endereco}
-        placeholder="Endereço"
-        keyboardType="default"
-        returnKeyType="next"
-        onChangeText={t => setEndereco(t)}
+        <Text style={{ color: 'black', fontSize: 18 }} >Localização</Text>
+        <View style={{ width: '65%' }} >
+          {renderPlaceholder(endereco ? 'Endereço' : '')}
+        </View>
+        <TextInput
+          placeholderTextColor="gray"
+          value={endereco}
+          placeholder="Endereço"
+          keyboardType="default"
+          returnKeyType="next"
+          onChangeText={t => setEndereco(t)}
         />
-              <View  style={{ width: '65%' }} >
-      {renderPlaceholder(endereco ? 'Cidade' : '')}
-      </View>
-      <TextInput
-        placeholderTextColor="gray"
-        value={cidade}
-        placeholder="Cidade"
-        keyboardType="default"
-        returnKeyType="next"
-        onChangeText={t => setCidade(t)}
+        <View style={{ width: '65%' }} >
+          {renderPlaceholder(endereco ? 'Cidade' : '')}
+        </View>
+        <TextInput
+          placeholderTextColor="gray"
+          value={cidade}
+          placeholder="Cidade"
+          keyboardType="default"
+          returnKeyType="next"
+          onChangeText={t => setCidade(t)}
         />
-      <MeuButton texto="Salvar" onClick={salvar} />
-      {loading && <Loading />}
-    </Container>
+        <MeuButton texto="Salvar" onClick={salvar} />
+        {loading && <Loading />}
+      </Container>
+    </ScrollView>
   );
 };
 
